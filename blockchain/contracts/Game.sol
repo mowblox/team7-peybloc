1c2d6546d0157c0a9d2dcb6fe6f067216d2f07f2
yawoffeh <yawoffeh123@gmail.com>
2023-08-10 11:52:23 +0000

    update milestone 1

---
+++ blockchain/contracts/Game.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Lock {
+    function mintAsset(address buyerAddress, string memory URI) {
+        // mint new nft
+    }    
+
+    function buyAsset() {
+	// check buyer balance
+	// deduct amount if user has enough money
+	// send asset to buyer	
+    // Add transaction history
+    }
+}
